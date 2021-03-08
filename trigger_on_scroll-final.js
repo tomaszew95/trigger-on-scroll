30e87f3659570b70c82b649c2ee017c587cdbaed
tomaszew95 <50740828+tomaszew95@users.noreply.github.com>
2021-03-08 10:28:13 +0100

    querySelectorAll

--- trigger_on_scroll-final.js
+++ trigger_on_scroll-final.js
@@ -28,7 +28,8 @@ var pageWidth = 1280;
 })();
 var triggerOnScroll = ($this, scrollObj, page) =>{
     let pageScroll = $(page).children().first();
-    let anchors = $(pageScroll).children(".scranchor").toArray();
+    let anchors = pageScroll.querySelectorAll(".scranchor");
+    console.log(anchors);
 
     for(let i = 0;i<scrollObj.length;i++){
         let tags = scrollObj[i].getTags();
