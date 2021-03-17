15196015da3aca2d0f54130d4cf249279b44c6f4
tomaszew95 <50740828+tomaszew95@users.noreply.github.com>
2021-03-17 12:03:46 +0100

    console.log()removed

--- trigger_on_scroll.js
+++ trigger_on_scroll.js
@@ -21,7 +21,6 @@ var pageWidth = 1280;
                 experience.on(CerosSDK.EVENTS.PAGE_CHANGED, pageChangedCallback);
                 function pageChangedCallback(){
                     console.log(scrollObjects);
-                    console.log(scrollObjects.toArray());
                     var pageContainer = document.querySelector(".page-viewport.top > .page-container");
                     var scrollObjs = scrollObjects.filter(($object) =>{
                         console.log('works');
