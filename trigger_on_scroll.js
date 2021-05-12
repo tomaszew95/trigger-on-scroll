59b6ee8bb2b1bde8979d2b784f4ef327b07a6091
tomaszew95 <50740828+tomaszew95@users.noreply.github.com>
2021-05-12 12:31:35 +0200

    update

--- trigger_on_scroll.js
+++ trigger_on_scroll.js
@@ -1,6 +1,4 @@
 var scrollPlugin = document.getElementById("ceros-trigger-on-scroll-plugin");
-var slideHeight = scrollPlugin.getAttribute("slide-height");
-var pageWidth = 1280;
 var anchors, currentPageScrollObjects;
 var objPosX = [], objPosY = [];
 (function(){
@@ -17,7 +15,6 @@ var objPosX = [], objPosY = [];
             })
             .done(function (experience) {
                 window.myExperience = experience;
-                pageWidth = experience.getCurrentPage().getWidth();
                 var scrollObjects = experience.findLayersByTag("scroll-effect").layers;
 
                 experience.on(CerosSDK.EVENTS.PAGE_CHANGED, pageChangedCallback);
@@ -60,16 +57,12 @@ var triggerOnScroll = ($this, scrollObj) =>{
         var directions = [];
         var firstAnchor = 0;
         var lastAnchor = ((anchors.length)-1);
+        var effectMultiplier = parseFloat(scrollPlugin.getAttribute("effect-multiplier"));
 
         _.forEach(tags, function(value, key){
             if(value.indexOf("move-direction:") > -1){
                 var direction = value.slice(15,value.length);
-                if(direction.search("&")>-1){
-                    directions = direction.split("&");
-                }
-                else{
-                    directions.push(direction);
-                }
+                directions = direction.split("&");
             }
             if(value.indexOf("min-scroll:") > -1){
                 firstAnchor = parseInt(value.slice(11,value.length), 10);
@@ -77,8 +70,11 @@ var triggerOnScroll = ($this, scrollObj) =>{
             if(value.indexOf("max-scroll:") > -1){
                 lastAnchor = parseInt(value.slice(11,value.length), 10);
             }
-            if(value.indexOf("scroll-delay:")>-1){
-                let duration = parseFloat(value.slice(13,value.length));
+            if(value.indexOf("multiplier:") > -1){
+                effectMultiplier = parseFloat(value.slice(11,value.length));
+            }
+            if(value.indexOf("scroll-duration:")>-1){
+                let duration = parseFloat(value.slice(16,value.length));
                 let dur = 'top ' + duration + 'ms ease, left ' + duration + 'ms ease';
                 obj.style.setProperty("transition", dur);
             }
@@ -111,7 +107,7 @@ var triggerOnScroll = ($this, scrollObj) =>{
 
         //scroll position is between Ceros anchors
         if(scrollPosition >= minScroll && scrollPosition <= maxScroll){
-            obj.style.setProperty('left',(objPosX[i]+(differencePos*(pageWidth/slideHeight)*scrollX))+'px');
+            obj.style.setProperty('left',(objPosX[i]+(differencePos*effectMultiplier*scrollX))+'px');
             obj.style.setProperty('top',(objPosY[i]+(differencePos*scrollY))+'px');
         }
         //scroll position is above first Ceros anchor
@@ -121,7 +117,7 @@ var triggerOnScroll = ($this, scrollObj) =>{
         }
         //scroll position is below second Ceros anchor
         else{
-            obj.style.setProperty('left',(objPosX[i]+(scrollRange*(pageWidth/slideHeight)*scrollX))+'px');
+            obj.style.setProperty('left',(objPosX[i]+(scrollRange*effectMultiplier*scrollX))+'px');
             obj.style.setProperty('top',(objPosY[i]+(scrollRange*scrollY))+'px');
         }
     }
